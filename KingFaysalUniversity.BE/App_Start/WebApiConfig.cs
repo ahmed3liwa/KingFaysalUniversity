28fd01d6486359ab52d695a66e7b4c4224434bc5
EMEINT\ahmedm <ahmedm>
2018-06-30 19:35:29 +0200

    reset bransh

--- KingFaysalUniversity.BE/App_Start/WebApiConfig.cs
+++ KingFaysalUniversity.BE/App_Start/WebApiConfig.cs
@@ -9,6 +9,7 @@ namespace KingFaysalUniversity.BE
     {
         public static void Register(HttpConfiguration config)
         {
+            // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
