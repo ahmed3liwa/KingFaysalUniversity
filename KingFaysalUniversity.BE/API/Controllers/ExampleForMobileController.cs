9a2b4e47395de49724d2bb15d8d8b81447201449
EMEINT\ahmedm <ahmedm>
2018-06-30 19:33:47 +0200

    test conmnection between locale and remote

---
+++ KingFaysalUniversity.BE/API/Controllers/ExampleForMobileController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace KingFaysalUniversity.BE.API.Controllers
+{
+    public class ExampleForMobileController : ApiController
+    {
+    }
+}
